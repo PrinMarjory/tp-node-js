199919803b0123e43c9cf9f665684764612ecab1
Marjory PRIN <marjory.prin@gmail.com>
2024-06-27 15:56:06 +0200

    Exo 3 - Readline (OS)

---
+++ 2--readline/exo-3.js
@@ -0,0 +1,37 @@
+/* EXERCICE OS
+1/ Le programme demande à l'utilisateur quelle info il veut sur son OS
+2/ Le programme lui affiche l'info et repart en 1/ tant que 'exit' nest pas entré */
+
+const rl = require("./readlineModule");
+const os = require("os");
+
+function askInfo() {
+    rl.question("Quelle information voulez-vous sur votre système d'exploitation? (Tapez 'exit' pour quitter) : ", (input) => {
+        if (input.toLowerCase() === 'exit') {
+            return rl.close();
+            
+        }
+
+        switch (input.toLowerCase()) {
+            case 'hostname':
+                console.log(`Nom de l'hôte : ${os.hostname()}`);
+                break;
+            case 'type':
+                console.log(`Type de système d'exploitation : ${os.type()}`);
+                break;
+            case 'platform':
+                console.log(`Plateforme : ${os.platform()}`);
+                break;
+            case 'arch':
+                console.log(`Architecture du processeur : ${os.arch()}`);
+                break;
+            default:
+                console.log("Information non valide. Veuillez entrer 'hostname', 'type', 'platform', 'arch' ou 'exit'.");
+                break;
+        }
+
+        askInfo(); 
+    });
+}
+
+askInfo();
